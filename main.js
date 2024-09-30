96ad57df8b7ecb07e5465639a429522591364637
rosidin45 <muhamadrosidin233@gmail.com>
2024-09-30 10:22:52 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
-import { 
+import {
   getFirestore,
   collection,
   doc,
@@ -10,51 +10,51 @@ import {
   updateDoc,
   query,
   orderBy
-  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 
+// ini adalah konfigurasi firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCgSS-chZUH5T47nhRNeK6jYDnGZK_TQSA",
-  authDomain: "insan-cemerlang-d6eb1.firebaseapp.com",
-  projectId: "insan-cemerlang-d6eb1",
-  storageBucket: "insan-cemerlang-d6eb1.appspot.com",
-  messagingSenderId: "162904381844",
-  appId: "1:162904381844:web:dd88782fdcc494c9ac1781",
-  measurementId: "G-1RSX6TCWZ2"
+  apiKey: "AIzaSyDFYmmVvk-jLZIeAdYKiTwVw2jqd4VINFA",
+  authDomain: "insan-cemerlang.firebaseapp.com",
+  projectId: "insan-cemerlang",
+  storageBucket: "insan-cemerlang.appspot.com",
+  messagingSenderId: "579109661574",
+  appId: "1:579109661574:web:4a7cd4060f70eded945a07"
 };
 
-// Inisialisasi Firebase
+// Inisialisasi firebase
 const aplikasi = initializeApp(firebaseConfig)
 const basisdata = getFirestore(aplikasi)
 
-export async function ambilDaftarSiswa() {
- const refDokumen = collection(basisdata, "siswa");
- const kueri = query(refDokumen, orderBy("nama"));
- const cuplikanKueri = await getDocs(kueri);
- 
- let hasilKueri = [];
- cuplikanKueri. forEach((dokumen) => {
-  hasilKueri.push({
-    id:dokumen.id,
-    nama: dokumen.data().nama, 
-    alamat: dokumen.data().alamat
+export async function ambildatapelanggan() {
+  const refDokumen = collection(basisdata, "pelanggan");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      nama: dokumen.data().nama,
+      notelpon: dokumen.data().notelpon
+    })
   })
- })
- 
- return hasilKueri;
+
+  return hasilKueri;
 }
 
-export async function tambahsiswa(nama, alamat){
+export async function tambahpelanggan(nama, notelpon) {
   try {
-  // menyimpan data ke firebase
-  const refDokumen = await addDoc(collection(basisdata, "siswa"),{
-    nama: nama, 
-    alamat: alamat
-  })
-  
-  // menampilkan pesan berhasil
-  console. log ("berhasil menyimpan data siswa")
-  } catch ( error) {
-    //menampilkan pesan gagal
-    console.log("gagal menyimpan data siswa")
+    // menyimpan data ke firebase
+    const refDokumen = await addDoc(collection(basisdata, "pelanggan"), {
+      nama: nama,
+      notelpon: notelpon
+    })
+
+    // menampilkan pesan berhasil
+    console.log("berhasil menyimpan data pelanggan")
+  } catch (error) {
+    // menampilkan pesan gagal
+    console.log("gagal menyimpan data pelanggan")
   }
-  }
+}
